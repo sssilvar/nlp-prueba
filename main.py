c021f306524205c61bb9b56e84c7536e2d8983a4
Santiago Smith <sssilvar@unal.edu.co>
2019-09-08 22:23:59 -0500

    Correcciones añadidas

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def extract_data_vectors(filename, training_sheets, testing_sheets):
              y contiene las etiquetas (y_test no es proveído)
     """
     # Crear un DataFrame de entrenamiento y prueba vacíos
-    df_train = df_test = pd.DataFrame()
+    df_train = pd.DataFrame()
     xls = pd.ExcelFile(filename)  # Objeto Excel
 
     # Leer datos de entrenamiento
@@ -34,9 +34,10 @@ def extract_data_vectors(filename, training_sheets, testing_sheets):
         df_train = df_train.append(df, ignore_index=True, sort=False)
 
     # Leer datos de prueba
+    df_test = pd.DataFrame()
     for sheet in testing_sheets:
         df = pd.read_excel(xls, sheet)
-        df_test = df_train.append(df, ignore_index=True, sort=False)
+        df_test = df_test.append(df, ignore_index=True, sort=False)
 
     X_train, y_train = df_train['P6390'], df_train['RAMA2D_R4']
     X_test = df_test['P6390']
@@ -64,6 +65,17 @@ def create_pipeline():
     ])
     return pipeline
 
+def extract_sample(dataframe):
+    """
+    Extrae una muestra aleatoria si la cantidad de observaciones por cada clase es mayor a 5.
+    :param dataframe: Pandas DataFrame
+    :return: (pandas.DataFrame) muestra aleatoria de 5 elementos
+    """
+    if len(dataframe) > 5:
+        return dataframe.sample(5)
+    else:
+        return dataframe.head()
+
 
 if __name__ == '__main__':
     # Leer el archivo Excel que contiene las respuestas
@@ -73,6 +85,9 @@ if __name__ == '__main__':
                                         testing_sheets=['validacion'])
     df_train = pd.concat([X, y], axis='columns')
     print(df_train.head())
+    print(f'Training dataset samples: {len(X)}')
+    print(f'Testing dataset samples: {len(X_test)}')
+    # exit()
 
     # Instanciar y entrenar pipeline
     pipeline = create_pipeline()
@@ -89,19 +104,28 @@ if __name__ == '__main__':
     categories = y.value_counts().index
     sep = '='
     for category in categories:
-        df_test_head = df_pred.query(f'RAMA2D_R4 == {category}').head()
-        df_train_head = df_train.query(f'RAMA2D_R4 == {category}').head()
+        # Filtrar por categoría
+        df_test_query = df_pred.query(f'RAMA2D_R4 == {category}')
+        df_train_query = df_train.query(f'RAMA2D_R4 == {category}')
 
+        # Extraer muestra aleatoria si hay más de 5
+        # observaciones en la clase
+        df_test_head = extract_sample(df_test_query)
+        df_train_head = extract_sample(df_train_query)
+
+        # Imprimir resultados
         print(f'{sep * 10} PPREDICTED {sep * 10}')
         print(df_test_head)
         print(f'{sep * 10} GROUNDTRUTH {sep * 10}')
         print(df_train_head)
         print('\n\n')
 
+        # Guardar dataframes como imágenes
+        # (para reporte y presentación)
         try:
             dataframe_to_image(data=df_test_head,
                                outputfile=f'/tmp/{category}_test.png')
-            dataframe_to_image(data=df_test_head,
+            dataframe_to_image(data=df_train_head,
                                outputfile=f'/tmp/{category}_train.png')
         except:
             pass
